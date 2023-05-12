facf566b15f4dd282165810769b7d94335afc38d
radjaho973 <adjaho.b@hotmail.fr>
2023-05-12 18:12:22 -0300

    ajout de variable twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,8 +14,10 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(FormatRepository $formatRepo, CategorieRepository $categorieRepo, PostRepository $postRepo): Response
     {
+        $latestPost = $postRepo->findBy([],["id" => "DESC"], 20);
 
         return $this->render('home/index.html.twig', [
+            'latest_post' => $latestPost,
             'controller_name' => 'HomeController',
         ]);
     }
