65d4daa74207854f8b070273fbd8d41fecb243d0
radjaho973 <adjaho.b@hotmail.fr>
2023-05-19 18:26:47 -0300

    ajout de la table media en bdd

---
+++ migrations/Version20230519212458.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230519212458 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE media (id INT AUTO_INCREMENT NOT NULL, fk_post_id INT NOT NULL, alt VARCHAR(255) DEFAULT NULL, nom VARCHAR(255) NOT NULL, vignette VARCHAR(255) NOT NULL, src VARCHAR(255) NOT NULL, extension VARCHAR(255) NOT NULL, INDEX IDX_6A2CA10CBBA63E00 (fk_post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10CBBA63E00 FOREIGN KEY (fk_post_id) REFERENCES post (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE media DROP FOREIGN KEY FK_6A2CA10CBBA63E00');
+        $this->addSql('DROP TABLE media');
+    }
+}
